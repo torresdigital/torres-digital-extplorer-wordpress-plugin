9c6dd9a4089e2c547ccd40b9dac3de2775fc1c52
atorresbr <atorresnh@gmail.com>
2020-11-12 18:04:25 -0300

    Update torres-digital-extplorer-wordpressp-plugin.php

--- torres-digital-extplorer-wordpressp-plugin.php
+++ torres-digital-extplorer-wordpressp-plugin.php
@@ -1,24 +1,24 @@
 <?php /* The Silence is Gold!
 
-Plugin Name: Torres Digital - ICECoder IDE WP Plugin
+Plugin Name: Torres Digital - eXtPlorer IDE WP Plugin
 Plugin URI: http://torresdigital.com.br/
-Description: ❉ This plugin allows the <strong>ICECoder IDE</strong> to be accessed directly through the Wordpress Panel. Simple as that, simple as life should be. Source Code on GitHub https://github.com/icecoder/ICEcoder | ICECoder IDE Site - https://icecoder.net/
+Description: ❉ This plugin allows the <strong>eXtPlorer IDE</strong> to be accessed directly through the Wordpress Panel. Simple as that, simple as life should be. Source Code on GitHub https://github.com/eXtPlorer/eXtPlorer | eXtPlorer IDE Site - https://eXtPlorer.net/
 Version: 2.2.0.2020
 Author: Torres Digital -Sites → Lojas Virtuais e e-Commerce
 Author URI: https://facebook.com/torresdigital */
 
      /**
-        *Torres Digital ICECoder IDE WP Plugin By Torres Digital ! // Source https://extreme-ip-lookup.com/
+        *Torres Digital eXtPlorer IDE WP Plugin By Torres Digital ! // Source https://extreme-ip-lookup.com/
         *
         */
         add_action('admin_bar_menu', 'add_toolbar_items', 100);
         function add_toolbar_items($admin_bar){
             $admin_bar->add_menu( array(
                 'id'    => 'torres-digital-extplorer-wordpress-plugin',
-                'title' => ' ❉ ICECoder IDE - LOGIN',
-                'href'  => '/wp-content/plugins/torres-digital-torres-digital-extplorer-wordpress-plugin-wp-plugin-master/ICEcoder-master/lib/login.php',
+                'title' => ' ❉ eXtPlorer - LOGIN',
+                'href'  => '/wp-content/plugins/torres-digital-torres-digital-extplorer-wordpress-plugin-wp-plugin-master/eXtPlorer-master/lib/login.php',
                 'meta'  => array(
-                    'title' => __('* ICECoder IDE - LOGIN'),
+                    'title' => __('* eXtPlorer - LOGIN'),
                     'target' => '_blank',
                     'class' => 'torres-digital-extplorer-wordpress-plugin'
                 ),
@@ -64,8 +64,8 @@ add_action( 'wp_enqueue_scripts', 'wmpudev_enqueue_icon_stylesheet' );
 function wpse_load_plugin_css() {
     $plugin_url = plugin_dir_url( __FILE__ );
 
-    wp_enqueue_style( 'style', $plugin_url . 'css/style.css' );
-    wp_enqueue_style( 'style2', $plugin_url . 'css/style2.css' );
+    wp_enqueue_style( 'style', $plugin_url . 'assets/css/style.css' );
+    wp_enqueue_style( 'style2', $plugin_url . 'assets/css/style2.css' );
 
 }
 add_action( 'wp_enqueue_scripts', 'wpse_load_plugin_css' );
