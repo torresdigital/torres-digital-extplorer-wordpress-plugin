e91086fa79871a0767b4ba70ef370182ee7a6831
atorresbr <atorresnh@gmail.com>
2020-11-13 17:01:34 -0300

    Rocket ! 🚀

--- torres-digital-extplorer-wordpressp-plugin.php
+++ torres-digital-extplorer-wordpressp-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Torres Digital - eXtPlorer WP Plugin
 Plugin URI: http://torresdigital.com.br/
 Description: ❉ This plugin allows the <strong>eXtPlorer </strong> to be accessed directly through the Wordpress Panel. Simple as that, simple as life should be. Source Code on GitHub https://github.com/eXtPlorer/eXtPlorer | eXtPlorer  Site - https://eXtPlorer.net/
-Version: 1.0
+Version: 1.0.0
 Author: Torres Digital -Sites → Lojas Virtuais e e-Commerce
 Author URI: https://facebook.com/torresdigital */
 
