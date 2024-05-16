a385db29b3b3f500534889b2ee642e48315af588
Anderson Torres <atorresnh@gmail.com>
2024-05-16 14:59:27 -0300

    New version to 2024

--- torres-digital-extplorer-wordpressp-plugin.php
+++ torres-digital-extplorer-wordpressp-plugin.php
@@ -1,6 +1,6 @@
 <?php /* The Silence is Gold!
 
-Plugin Name: Torres Digital - eXtPlorer Wordpress Plugin
+Plugin Name: Torres Digital - eXtPlorer Wordpress Plugin 🟢
 Plugin URI: http://torresdigital.com.br
 Description: ❉ This plugin allows the <strong>eXtPlorer </strong> to be accessed directly through the Wordpress Panel. Simple as that, simple as life should be. Source Code on GitHub https://github.com/torresdigital/torres-digital-eXtPlorer-wordpress-plugin eXtPlorer  Site - https://extplorer.net/
 Version: 2.2.4 - 2024
@@ -50,6 +50,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                 ),
             ));
         }
+
 /**
  * Font Awesome Kit Setup
  *
@@ -91,4 +92,3 @@ function extplorer_load_plugin_css() {
 add_action( 'wp_enqueue_scripts', 'extplorer_load_plugin_css' );
 
 ?>
-
